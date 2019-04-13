29049c30444e0a050948e2b327c5b5ffa61d1f5c
Saikat <saikats_5@yahoo.co.in>
2019-04-13 16:16:33 +0530

    mongodb setup

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ npm init --yes // install without asking the answers, provide default data
 express is a web server
 body-parser is a middleware to handle form data such as user registration and login
 
+mLab for mongodb -- https://mlab.com/ -- admin1
+mongoose provide mongodb object mapping
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const db = "mongodb://admin1:admin1@ds135796.mlab.com:35796/angauthdb";
+
+mongoose.connect(db, err => {
+    if (err) {
+        console.log('ERROR!' + err);
+    } else {
+        console.log('Connected to MongoDB');
+    }
+});
 
 router.get('/', (req, res) => {
     res.send('From API route');
