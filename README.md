af119f5edbb17c31dfe0084a969c7efdc6b6c107
Saikat <saikats_5@yahoo.co.in>
2019-04-15 09:25:20 +0530

    backend setup

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ body-parser is a middleware to handle form data such as user registration and lo
 
 mLab for mongodb -- https://mlab.com/ -- admin1
 mongoose provide mongodb object mapping
+
+Postman is used to test api without relying on UI, i.e., test user registration in this case
+www.getpostman.com
+
+Content types also known as MIME type or media types are a two part identifier for file formats. The HTTP header Content-Type is responsible for telling the HTTP client or server what type of data is being sent
+
+Content-Type -- application/json
+{
+	"email": "abc@abc.com",
+	"password": "abc"
+}
+
+status 401: The request has not been applied because it lacks valid authentication credentials for the target resource.
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 const mongoose = require('mongoose');
 const db = "mongodb://admin1:admin1@ds135796.mlab.com:35796/angauthdb";
 
@@ -13,6 +14,109 @@ mongoose.connect(db, err => {
 
 router.get('/', (req, res) => {
     res.send('From API route');
+})
+
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.status(200).send(registeredUser);
+        }
+    })
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body;
+    User.findOne({ email: userData.email }, (error, user) => {
+        if (error) {
+            console.log(error);
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid email');
+            } else {
+                if (user.password !== userData.password) {
+                    res.status(401).send('Invalid password');
+                } else {
+                    res.status(200).send(user);
+                }
+            }
+        }
+    })
+})
+
+router.get('/events', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "ABC",
+            "description": "abc",
+            "date": "01/01/2019"
+        },
+        {
+            "_id": "2",
+            "name": "DEF",
+            "description": "def",
+            "date": "02/02/2019"
+        },
+        {
+            "_id": "3",
+            "name": "GHI",
+            "description": "ghi",
+            "date": "03/03/2019"
+        },
+        {
+            "_id": "4",
+            "name": "JKL",
+            "description": "jkl",
+            "date": "04/04/2019"
+        },
+        {
+            "_id": "5",
+            "name": "MNO",
+            "description": "mno",
+            "date": "05/05/2019"
+        }
+    ]
+    res.json(events);
+})
+
+router.get('/special', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "ABC",
+            "description": "abc",
+            "date": "01/01/2019"
+        },
+        {
+            "_id": "2",
+            "name": "DEF",
+            "description": "def",
+            "date": "02/02/2019"
+        },
+        {
+            "_id": "3",
+            "name": "GHI",
+            "description": "ghi",
+            "date": "03/03/2019"
+        },
+        {
+            "_id": "4",
+            "name": "JKL",
+            "description": "jkl",
+            "date": "04/04/2019"
+        },
+        {
+            "_id": "5",
+            "name": "MNO",
+            "description": "mno",
+            "date": "05/05/2019"
+        }
+    ]
+    res.json(events);
 })
 
 module.exports = router;
