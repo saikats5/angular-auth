0b413765b93fe386d5aa1921868f5ba90f0ad316
Saikat <saikats_5@yahoo.co.in>
2019-04-15 11:05:04 +0530

    service setup

--- authapp/src/app/app-routing.module.ts
+++ authapp/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'special', component: SpecialEventsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
