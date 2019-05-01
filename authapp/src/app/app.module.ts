c9b431fede42f21f15eb1da80f03ac29d01cd02f
Saikat <saikats_5@yahoo.co.in>
2019-05-01 19:50:25 +0530

    authorization

--- authapp/src/app/app.module.ts
+++ authapp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
@@ -11,6 +11,7 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
 import { AuthService } from './auth.service';
 import { EventService } from './event.service';
 import { AuthGuard } from './auth.guard';
+import { TokenInterceptorService } from './token-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,12 @@ import { AuthGuard } from './auth.guard';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [AuthService, AuthGuard, EventService],
+  providers: [AuthService, AuthGuard, EventService, 
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- authapp/src/app/token-interceptor.service.ts
+++ authapp/src/app/token-interceptor.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  constructor(private injector: Injector) { }
   intercept(req, next){
-    let tokenizedReq = req.clone({})
+    let authService = this.injector.get(AuthService)
+    let tokenizedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${authService.getToken()}`
+      }
+    })
+    return next.handle(tokenizedReq);
   }
 }
